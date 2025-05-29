719280b67de2e0a2e643579914798f38558608df
Gustavo <gustavohen028@gmail.com>
2025-05-29 18:10:20 -0400

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -3,7 +3,8 @@ const botao = document.getElementById('modoboxe');
 botao.addEventListener('click', () => {
     const atual = window.location.pathname;
 
-    if (atual.includes('index.html')) {
+    if (atual.endsWith('/') || atual.endsWith('/index.html')) {
+      
         window.location.href = 'modoboxe.html';
     } else if (atual.includes('modoboxe.html')) {
         window.location.href = 'index.html';
@@ -11,6 +12,7 @@ botao.addEventListener('click', () => {
         console.error('Página atual desconhecida.');
     }
 });
+
 
 window.addEventListener('scroll', function() {
   const navbar = document.querySelector('.custom-navbar');
