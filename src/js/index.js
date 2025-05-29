c79de896a19cbeb880469cb5007faa9191f3d0f0
Gustavo <gustavohen028@gmail.com>
2025-05-28 20:34:55 -0400

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,24 @@
+const botao = document.getElementById('modoboxe');
+
+botao.addEventListener('click', () => {
+    const atual = window.location.pathname;
+
+    if (atual.includes('index.html')) {
+        window.location.href = 'modoboxe.html';
+    } else if (atual.includes('modoboxe.html')) {
+        window.location.href = 'index.html';
+    } else {
+        console.error('Página atual desconhecida.');
+    }
+});
+
+window.addEventListener('scroll', function() {
+  const navbar = document.querySelector('.custom-navbar');
+  if (window.scrollY > 50) {
+    navbar.classList.add('scrolled');
+  } else {
+    navbar.classList.remove('scrolled');
+  }
+});
+ 
+  
