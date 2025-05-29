240692319d849df45fb2cdb9aaa4cff06db8f2a6
Gustavo <gustavohen028@gmail.com>
2025-05-28 21:04:00 -0400

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,29 +1,32 @@
-const botao = document.getElementById('modoboxe');
+document.addEventListener('DOMContentLoaded', () => {
+  const botao = document.getElementById('modoboxe');
 
-botao.addEventListener('click', () => {
-    const atual = window.location.pathname;
+  if (botao) {
+    botao.addEventListener('click', () => {
+      const atual = window.location.pathname;
 
-    if (atual.endsWith('/') || atual.endsWith('index.html')) {
-        // Está na página inicial
-        window.location.href = `${window.location.origin}${getBasePath()}/modoboxe.html`;
-    } else if (atual.endsWith('modoboxe.html')) {
-        // Está na página modoboxe
-        window.location.href = `${window.location.origin}${getBasePath()}/index.html`;
-    } else {
+      if (atual.endsWith('/') || atual.endsWith('index.html')) {
+        window.location.href = 'modoboxe.html';
+      } else if (atual.endsWith('modoboxe.html')) {
+        window.location.href = 'index.html';
+      } else {
         console.error('Página atual desconhecida:', atual);
+      }
+    });
+  }
+
+  window.addEventListener('scroll', function() {
+    const navbar = document.querySelector('.custom-navbar');
+    if (navbar) {
+      if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+      } else {
+        navbar.classList.remove('scrolled');
+      }
     }
+  });
 });
 
-// Função para obter o caminho base do repositório no GitHub Pages
-function getBasePath() {
-    const pathParts = window.location.pathname.split('/');
-    // Se o site estiver hospedado em /usuario.github.io/repositorio/
-    // então o path base será "/repositorio"
-    if (pathParts.length > 1 && pathParts[1] !== '') {
-        return '/' + pathParts[1];
-    }
-    return '';  // caso esteja na raiz
-}
 
 window.addEventListener('scroll', function() {
     const navbar = document.querySelector('.custom-navbar');
